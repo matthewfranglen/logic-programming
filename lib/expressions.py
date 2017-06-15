e29919d119b10ebca37c892b98f5a161afcf7f82
Matthew Franglen <matthew@franglen.org>
2017-06-16 00:34:12 +0100

    Add docstring
    And trivial get_children implementation which, no doubt, will have to
    change.

--- lib/expressions.py
+++ lib/expressions.py
@@ -1,17 +1,25 @@
-def is_parent(events, people, child, parent):
-    return bool([
-        event
+"""
+Expressions form the search which can be applied to the Fact graph.
+
+The aim is to represent the search as an expression.
+An expression is formed of placeholders, relationships and established values.
+
+Placeholders represent values which are yet to be determined.
+Without the constraint applied by relationships, all people are valid for a given placeholder.
+
+Relationships represent connections between people.
+Relationships can be substituted by compatible expressions.
+This substitution can be infinite, so relationship expansion is handled by generators.
+
+Established values are reified placeholders.
+They only match a single person.
+"""
+
+
+def get_children(events, _, parent):
+    return [
+        event.child
         for event in events
-        if event['type'] == 'birth'
-            and child == event['child']
-            and parent in event['parents']
-    ])
-
-def is_ancestor(events, people, person, ancestor):
-    pass
-
-def is_descendant(events, people, person, descendant):
-    return is_ancestor(events, people, descendant, person)
-
-def is_sibling(events, people, person, sibling):
-    pass
+        if event.type == 'birth'
+        and parent in event.parents
+    ]
