df8cc0e6147416ca8b14728a97a2323394aecfae
Matthew Franglen <matthew@brandwatch.com>
2017-06-15 12:57:14 +0100

    Add some blank expressions

--- lib/expressions.py
+++ lib/expressions.py
@@ -6,3 +6,12 @@ def is_parent(events, people, child, parent):
             and child == event['child']
             and parent in event['parents']
     ])
+
+def is_ancestor(events, people, person, ancestor):
+    pass
+
+def is_descendant(events, people, person, descendant):
+    return is_ancestor(events, people, descendant, person)
+
+def is_sibling(events, people, person, sibling):
+    pass
