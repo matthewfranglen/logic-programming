0c23e698878b83eab91fc88d6b8ddb8d6a5fff82
Matthew Franglen <matthew@brandwatch.com>
2017-06-23 09:17:52 +0100

    Double down on the madness
    The state is required however this has become a solution which is both
    overly specific in it's implementation and overly general in it's
    performance.
    
    It's time to rewrite this using actual graphs and graph matching.

---
+++ lib/state.py
@@ -0,0 +1,6 @@
+"""
+State is the combination of the values and the discrete expressions.
+
+The expressions represent constraints over the values, and multiple expressions can operate over the same values.
+So the state collects together these different constraints and allows them to be symultaneously satisfied.
+"""
